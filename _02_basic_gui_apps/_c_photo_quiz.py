4c70c3d4c5b858b84c81723c07df434cb3b994fd
league <asherwelsbie@icloud.com>
2024-04-28 12:57:28 -0700

    end of photo quiz, start of dueling buttons

--- _02_basic_gui_apps/_c_photo_quiz.py
+++ _02_basic_gui_apps/_c_photo_quiz.py
@@ -29,32 +29,48 @@ class SecondApp(tk.Tk):
     # TODO 2) Create a constructor
     def __init__(self):
         # TODO 3) call Tk's constructor
-        super.__init__()
+        super().__init__()
         # TODO 4) Create a member variable for a label and place it.
         #  You do not need to add any text or images to the label.
-
-
+        self.photo_label = tk.Label()
+        self.photo_label.place(x=0, y=0)
 # TODO 5) Create an if __name__ == '__main__': block
 if __name__ == '__main__':
     # TODO 6) Create an object of the tkinter class
-    tk = SecondApp()
+    app = SecondApp()
     # TODO 7) Set the app window width and height using geometry()
-    tk.geometry("500x500")
+    app.geometry("500x500")
     # TODO 8) Declare and initialize a score variable
-
+    app.score = 0
     # TODO 9) Create an image object variable using the create_image function
     #  above and store it in a variable
-
+    image_object = create_image("carrots.jpg", 400, 400)
     # TODO 10) Set the image onto the class's label using the configure method,
     #  for example:
-    #  app.photo_label.configure(image=image_object)
+    app.photo_label.configure(image=image_object)
 
     # TODO 11) Use a pop-up (simpledialog) to ask the user a question
     #  relating to the image and tell them if they get the right answer.
+    question = simpledialog.askstring(title=None, prompt="What is this?")
 
     # TODO 12) If the answer is correct, increase the score by 1
-
+    if question == "carrots":
+        app.score += 1
     # TODO 13) Repeat the steps to show a different photo and ask a different
     #  question
+    image_object2 = create_image("fossil.jpg", 400, 400)
+    app.photo_label.configure(image=image_object2)
+    question2 = simpledialog.askstring(title=None, prompt="What is this?")
+
+    if question2 == "fossil":
+        app.score += 1
+
+    image_object3 = create_image("python.png", 400, 400)
+    app.photo_label.configure(image=image_object3)
+    question3 = simpledialog.askstring(title=None, prompt="What is this?")
+
+    if question3 == "python":
+        app.score += 1
 
     # TODO 14) Display the score to the user after asking the last question
+    messagebox.showinfo(title=None, message="Score: " + str(app.score))
--- _03_more_gui_apps/_b_dueling_buttons.py
+++ _03_more_gui_apps/_b_dueling_buttons.py
@@ -11,29 +11,40 @@ RIGHT_SMALL = {'relx' : 0.8, 'relwidth' : 0.2}
 # ======================= DO NOT EDIT THE CODE ABOVE =========================
 
 # TODO 1) Create a new tkinter class
-
+class ThirdApp(tk.Tk):
     # TODO 2) Create a constructor
+    def __init__(self):
 
+        super().__init__()
     # TODO 3) call Tk's constructor,
 
     # TODO 4) Create 2 buttons next to each other, side by side, with the
     #  text "Click me!". Make sure the buttons are member variables and
     #  look at the example photo for reference.
+        #self.button = tk.Button(self, text='Click me!', fg='blue',
+                                #font=('courier new', 16, 'bold'), command=self.left_button_pressed)
+        #self.button.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
 
+        #self.button2 = tk.Button(self, text='Click me!', fg='blue',
+                                #font=('courier new', 16, 'bold'), command=self.right_button_pressed)
+        #self.button2.place(relx=0.5, rely=0.5, relwidth=0.8, relheight=0.2)
     # TODO 5) For the left button, add: command=self.left_button_pressed
     #  For the right button, add: command=self.right_button_pressed
 
     # TODO 6) Create a method called left_button_pressed that places the left
     #  button to small and the right button to big. For example,
-    #  self.button_left.place(LEFT_SMALL)
-    #  self.button_right.place(RIGHT_BIG)
+        #def left_button_pressed():
+            #self.button.place(LEFT_SMALL)
+            #self.button2.place(RIGHT_BIG)
 
     # TODO 7) Create a method called right_button_pressed and place the left
     #  button to big and the right button to small
-
+        #def right_button_pressed(self):
+            #self.button.place(LEFT_BIG)
+            #self.button2.place(RIGHT_SMALL)
 
 # TODO 8) Create an if __name__ == '__main__': block
-
+    #if __name__ == '__main__':
     # TODO 9) Create an object of the dueling buttons class
 
     # TODO 10) Set the object's width and size using the geometry method
