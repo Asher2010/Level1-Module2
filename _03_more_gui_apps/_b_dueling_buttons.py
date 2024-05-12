c326773890ba68b441cbfa628fa3bf2390f8c0a3
league <asherwelsbie@icloud.com>
2024-05-12 12:55:13 -0700

    end of dueling buttons, start of calculator

--- _03_more_gui_apps/_b_dueling_buttons.py
+++ _03_more_gui_apps/_b_dueling_buttons.py
@@ -11,7 +11,7 @@ RIGHT_SMALL = {'relx' : 0.8, 'relwidth' : 0.2}
 # ======================= DO NOT EDIT THE CODE ABOVE =========================
 
 # TODO 1) Create a new tkinter class
-class ThirdApp(tk.Tk):
+class Dueling_Buttons(tk.Tk):
     # TODO 2) Create a constructor
     def __init__(self):
 
@@ -21,32 +21,36 @@ class ThirdApp(tk.Tk):
     # TODO 4) Create 2 buttons next to each other, side by side, with the
     #  text "Click me!". Make sure the buttons are member variables and
     #  look at the example photo for reference.
-        #self.button = tk.Button(self, text='Click me!', fg='blue',
-                                #font=('courier new', 16, 'bold'), command=self.left_button_pressed)
-        #self.button.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
+        self.button = tk.Button(self, text='Click me!', fg='blue',
+                                font=('courier new', 16, 'bold'), command=self.left_button_pressed)
+        self.button.place(relx=0.0, rely=0.5, relwidth=0.8, relheight=0.2)
 
-        #self.button2 = tk.Button(self, text='Click me!', fg='blue',
-                                #font=('courier new', 16, 'bold'), command=self.right_button_pressed)
-        #self.button2.place(relx=0.5, rely=0.5, relwidth=0.8, relheight=0.2)
+        self.button2 = tk.Button(self, text='Click me!', fg='blue',
+                                font=('courier new', 16, 'bold'), command=self.right_button_pressed)
+        self.button2.place(relx=0.3, rely=0.5, relwidth=0.8, relheight=0.2)
     # TODO 5) For the left button, add: command=self.left_button_pressed
     #  For the right button, add: command=self.right_button_pressed
 
     # TODO 6) Create a method called left_button_pressed that places the left
     #  button to small and the right button to big. For example,
-        #def left_button_pressed():
-            #self.button.place(LEFT_SMALL)
-            #self.button2.place(RIGHT_BIG)
+    def left_button_pressed(self):
+        self.button.place(LEFT_SMALL)
+        self.button2.place(RIGHT_BIG)
 
     # TODO 7) Create a method called right_button_pressed and place the left
     #  button to big and the right button to small
-        #def right_button_pressed(self):
-            #self.button.place(LEFT_BIG)
-            #self.button2.place(RIGHT_SMALL)
+    def right_button_pressed(self):
+        self.button.place(LEFT_BIG)
+        self.button2.place(RIGHT_SMALL)
+
+
 
 # TODO 8) Create an if __name__ == '__main__': block
-    #if __name__ == '__main__':
-    # TODO 9) Create an object of the dueling buttons class
+if __name__ == '__main__':
+# TODO 9) Create an object of the dueling buttons class
+    dueling_buttons = Dueling_Buttons()
 
-    # TODO 10) Set the object's width and size using the geometry method
-
-    # TODO 11) Call the object's mainloop method
+# TODO 10) Set the object's width and size using the geometry method
+    dueling_buttons.geometry()
+# TODO 11) Call the object's mainloop method
+    dueling_buttons.mainloop()
--- _03_more_gui_apps/_c_calculator.py
+++ _03_more_gui_apps/_c_calculator.py
@@ -6,3 +6,36 @@ import tkinter as tk
 # TODO Make a calculator app like the one shown in the calculator.png image
 #  located in this folder.
 #  HINT: you'll need: 2 text fields, 4 buttons, and 1 label
+
+class Calculator(tk.Tk):
+    def __init__(self):
+
+        super().__init__()
+        self.button = tk.Button(self, text='Add', fg='black',
+                                font=('courier new', 16, 'bold'), command=self.on_button_press)
+        self.button.place(relx=0.0, rely=0.3, relwidth=0.2, relheight=0.1)
+
+        self.button2 = tk.Button(self, text='Subtract', fg='black',
+                                font=('courier new', 16, 'bold'), command=self.on_button_press)
+        self.button2.place(relx=0.2, rely=0.3, relwidth=0.3, relheight=0.1)
+
+        self.button3 = tk.Button(self, text='Multiply', fg='black',
+                                font=('courier new', 16, 'bold'), command=self.on_button_press)
+        self.button3.place(relx=0.5, rely=0.3, relwidth=0.3, relheight=0.1)
+
+        self.button4 = tk.Button(self, text='Divide', fg='black',
+                                font=('courier new', 16, 'bold'), command=self.on_button_press)
+        self.button4.place(relx=0.8, rely=0.3, relwidth=0.2, relheight=0.1)
+
+    def on_button_press(self):
+        pass
+
+
+        #self.text_field = tk.Entry(self)
+        #self.text_field.place(relx=0.1, rely=0.8, relwidth=0.8, height=18)
+
+if __name__ == '__main__':
+    app = Calculator()
+    app.title('Calculator')
+    app.geometry('300x300')
+    app.mainloop()
